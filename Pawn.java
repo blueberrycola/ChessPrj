53089edb4046c47a8944099e966aa13746999148
blueberrycola <46426781+blueberrycola@users.noreply.github.com>
2019-03-21 10:48:33 -0400

    Add files via upload

--- Pawn.java
+++ Pawn.java
@@ -11,14 +11,24 @@ public class Pawn extends ChessPiece {
 	}
 
 	public String type() {
-		return "Pawn";
+		return "W19Project3GIVETOSTUDENTS.Pawn";
 	}
 
 	// determines if the move is valid for a pawn piece
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
-
+		//FIXME: Attacking for pawn
 		boolean valid = true;
-        // More code is needed
-		return valid;
+		//Move up one
+		//model.pieceAt(r, c).player() == Player.WHITE
+		if(player() == Player.WHITE) {
+			if(move.fromRow - 1 == move.toRow && move.fromColumn == move.toColumn) {
+				return true;
+			}
+		} else {
+			if(move.fromRow + 1 == move.toRow && move.fromColumn == move.toColumn) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
