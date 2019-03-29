d5442285837e19fa5650f1a7772ebf1b0d4551fc
nicholasberens <47001009+nicholasberens@users.noreply.github.com>
2019-03-29 13:44:01 -0400

    Add files via upload

--- Pawn.java
+++ Pawn.java
@@ -6,14 +6,14 @@ import chess.Player;
 
 public class Pawn extends ChessPiece {
     private boolean firstTurn = true;
-	public Pawn(Player player) {
+    public Pawn(Player player) {
 
-	    super(player);
-	}
+        super(player);
+    }
 
-	public String type() {
-		return "W19Project3GIVETOSTUDENTS.Pawn";
-	}
+    public String type() {
+        return "W19Project3GIVETOSTUDENTS.Pawn";
+    }
 
     /***
      * The following method checks what moves are possible for a pawn chess piece. The firstTurn boolean value checks
@@ -25,48 +25,76 @@ public class Pawn extends ChessPiece {
      * @param board
      * @return True or False,
      */
-	public boolean isValidMove(Move move, IChessPiece[][] board) {
-		boolean valid = true;
-		//Move up one
-		//model.pieceAt(r, c).player() == Player.WHITE
-		if(player() == Player.WHITE) {
-		    //First turn check goes forward 2 units but only once
-		    if(move.fromRow - 2 == move.toRow && move.fromColumn == move.toColumn && firstTurn) {
-		        firstTurn = false;
-		        return true;
+    public boolean isValidMove(Move move, IChessPiece[][] board) {
+        boolean valid = true;
+        //Move up one
+        //model.pieceAt(r, c).player() == Player.WHITE
+        if(player() == Player.WHITE) {
+            //First turn check goes forward 2 units but only once
+            if(move.fromRow - 2 == move.toRow && move.fromColumn == move.toColumn && firstTurn) {
+                if(board[move.toRow][move.toColumn] != null || board[move.toRow + 1][move.toColumn] != null){
+                    return false;
+                }
+                firstTurn = false;
+                return true;
             }
 
-		    //Move up by one unit
-		    if(move.fromRow - 1 == move.toRow && move.fromColumn == move.toColumn) {
-				if(firstTurn) {
-				    firstTurn = false;
+            //Move up by one unit
+            if(move.fromRow - 1 == move.toRow && move.fromColumn == move.toColumn){
+                    //Return true if going to empty space
+                    if(board[move.toRow][move.toColumn] == null) {
+                        firstTurn = false;
+                        return true;
+                        //I have no idea why but without this empty if statement the whole thing will crash after one move
+                    }
+                    //Return false if attacking own piece
+                    else if (board[move.toRow][move.toColumn].player() != null) {
+                        return false;
+                    }
+                if(firstTurn) {
+                    firstTurn = false;
                 }
-		        return true;
-			}
-			//Move diagonally: ATTACK ONLY
-			if(move.fromRow - 1 == move.toRow) {
-			    if(move.fromColumn + 1 == move.toColumn || move.fromColumn - 1 == move.toColumn) {
+
+                return true;
+            }
+            //Move diagonally: ATTACK ONLY
+            if(move.fromRow - 1 == move.toRow) {
+                if(move.fromColumn + 1 == move.toColumn || move.fromColumn - 1 == move.toColumn) {
                     if(board[move.fromRow][move.fromColumn].player() == Player.WHITE && board[move.toRow][move.toColumn].player() == Player.BLACK) {
                         firstTurn = false;
                         return true;
                     }
-
                 }
             }
-		}
-		if(player() == Player.BLACK) {
-			//FIXME: Add first turn buffed move
-		    //First turn check
+        } else {
+            //FIXME: Add first turn buffed move
+            //First turn check
             if(move.fromRow + 2 == move.toRow && move.fromColumn == move.toColumn && firstTurn) {
+                if(board[move.toRow][move.toColumn] != null || board[move.toRow - 1][move.toColumn] != null){
+                    return false;
+                }
                 firstTurn = false;
                 return true;
             }
             //Move up by one unit
-		    if(move.fromRow + 1 == move.toRow && move.fromColumn == move.toColumn) {
-				firstTurn = false;
-				return true;
-			}
-			//Move diagonally: ATTACK ONLY
+            if(move.fromRow + 1 == move.toRow && move.fromColumn == move.toColumn){
+                //Return true if going to empty space
+                if(board[move.toRow][move.toColumn] == null) {
+                    firstTurn = false;
+                    return true;
+                    //I have no idea why but without this empty if statement the whole thing will crash after one move
+                }
+                //Return false if attacking own piece
+                else if (board[move.toRow][move.toColumn].player() != null) {
+                    return false;
+                }
+                if(firstTurn) {
+                    firstTurn = false;
+                }
+
+                return true;
+            }
+            //Move diagonally: ATTACK ONLY
             if(move.fromRow + 1 == move.toRow) {
                 if(move.fromColumn + 1 == move.toColumn || move.fromColumn - 1 == move.toColumn) {
                     if(board[move.fromRow][move.fromColumn].player() == Player.BLACK && board[move.toRow][move.toColumn].player() == Player.WHITE) {
@@ -75,7 +103,7 @@ public class Pawn extends ChessPiece {
                     }
                 }
             }
-		}
-		return false;
-	}
+        }
+        return false;
+    }
 }
